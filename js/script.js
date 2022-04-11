fa49a1a8af68bdc62a9fe9d499fa5dd5fcb82b39
Joel Wanjala <joel.n.wanjala@gmail.com>
2022-04-11 15:54:24 +0300

    Added some features to the website

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+/******************************
+CHANGE THE COPYRIGHT YEAR DYNAMICALLY
+*/
+const copyrightYear = document.querySelector('.copyright-year');
+
+const setCopyrightYear = function () {
+  const today = new Date().getFullYear();
+  copyrightYear.textContent = today;
+};
+
+setCopyrightYear();
