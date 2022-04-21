d4f033148975e33c3fb1174b23bd2d5649c2c92c
Joel Wanjala <joel.n.wanjala@gmail.com>
2022-04-21 15:41:43 +0300

    Responsiveness functionality

--- js/script.js
+++ js/script.js
@@ -11,3 +11,35 @@ const setCopyrightYear = function () {
 };
 
 setCopyrightYear();
+
+//////////////////////////////////////
+// SIDE NAV
+const navToggle = document.querySelector('.navigation__icon');
+const navBar = document.querySelector('.navigation__list');
+const navClose = document.querySelector('.side-nav__icon');
+const sideNav = document.querySelector('.side-nav');
+
+navToggle.addEventListener('click', function () {
+  sideNav.style.transform = `translateX(${0})`;
+});
+
+navClose.addEventListener('click', function () {
+  sideNav.style.transform = `translateX(${-100}%)`;
+});
+
+navBar.addEventListener('click', function () {
+  sideNav.style.transform = `translateX(${-100}%)`;
+});
+
+////////////////////////////////////////////////
+// FIXED NAVIGATION BAR
+const header = document.querySelector('.header');
+const nav = document.querySelector('.navigation');
+
+window.addEventListener('scroll', function () {
+  const scrollHeight = this.window.scrollY;
+  const navHeight = nav.getBoundingClientRect().height;
+
+  if (scrollHeight > navHeight) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+});
